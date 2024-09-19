815b434fa85d7b78b3c16e89e6a43659c7f231d5
daniel <danielpaivaborges@outlook.com>
2024-09-19 15:35:39 -0300

    REMESSA DA TARDE

---
+++ victor/projeto_perguntas/database/Pergunta.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const conexao = require("./database");
+
+const Pergunta = conexao.define("perguntas", {
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  descricao: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+});
+
+//cria a tabela, caso ela não exista
+Pergunta.sync({ force: false }).then(() =>
+  console.log("Tabela criada com sucesso.")
+);
+
+module.exports = Pergunta
