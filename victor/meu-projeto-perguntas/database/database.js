1e6af60797d90238947ea5e2369210f6446c273e
daniel <danielpaivaborges@outlook.com>
2024-09-20 13:56:41 -0300

    remessa integral da minha versão do projeto perguntas

---
+++ victor/meu-projeto-perguntas/database/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize')
+const connection = new Sequelize('everos', 'everos', 'root2024', {
+    host: 'mysql.everos.com.br',
+    dialect: 'mysql'
+})
+module.exports = connection
