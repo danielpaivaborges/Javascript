6bf6949f9b3b005eec696cf2a7de3b39bb79cd44
DANIEL PAIVA BORGES <danielpaivaborges@outlook.com>
2024-09-22 15:11:33 -0300

    sistema de domingo completo

---
+++ victor/meu_projeto_perguntas/database/Resposta.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const connection = require("./database");
+
+const Resposta = connection.define("respostas", {
+  descricao: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  idPergunta: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+Resposta.sync({force: false}).then(()=>console.log("Tabela Respostas criada com sucesso..."))
+
+module.exports = Resposta
---
+++ victor/meu_projeto_perguntas/database/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize')
+
+const connection = new Sequelize('everos', 'everos', 'root2024', {
+    host: 'mysql.everos.com.br',
+    dialect: 'mysql'
+})
+
+module.exports = connection
