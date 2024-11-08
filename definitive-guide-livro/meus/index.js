4d746c2184168e1c8aebde6a340696720de0efae
daniel <danielpaivaborges@outlook.com>
2024-11-08 14:43:56 -0300

    remessa novo livro

---
+++ definitive-guide-livro/meus/index.js
@@ -0,0 +1,4 @@
+let numeros = [4, 2, 0, 5, 9, 11, 6, 99, -4];
+
+numeros.sort((a,b)=>a-b)
+console.log(numeros);
--- victor/projeto_blog/users/UsersController.js
+++ victor/projeto_blog/users/UsersController.js
@@ -1,19 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const User = require('./User')
+const express = require("express");
+const router = express.Router();
+const User = require("./User");
+const bcrypt = require("bcryptjs");
 
-router.get('/admin/users', (req, res)=>{
-    res.send('lista de usuarios')
-})
+router.get("/admin/users", (req, res) => {
+  res.send("lista de usuarios");
+});
 
-router.get('/admin/users/create', (req, res)=>{
-  res.render('admin/users/create')
-})
+router.get("/admin/users/create", (req, res) => {
+  res.render("admin/users/create");
+});
 
-router.post('/users/create', (req, res)=>{
-  const email = req.body.email
-  const password = req.body.password
-  res.json({email, password})
-})
+router.post("/users/create", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
 
-module.exports = router
+  User.create({
+    email: email,
+    password: hash
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(() => {
+      res.redirect("/");
+    });
+});
+
+module.exports = router;
