2de149f6e4891e7457ba2fa005feac6212db0bbd
daniel <danielpaivaborges@outlook.com>
2024-11-13 07:56:24 -0300

    site atualizado

---
+++ definitive-guide-livro/meus/objetos.js
@@ -0,0 +1,9 @@
+"use strict";
+let livro = {
+    titulo: "o pequeno polegar",
+    ano_publicacao: '1960'
+}
+
+//formas de acessar
+console.log(livro.ano_publicacao)
+console.log(livro['ano_publicacao'])
---
+++ definitive-guide-livro/meus/tempo.js
@@ -0,0 +1,4 @@
+let numeros = [1,2,3,4,5]
+
+let confere = 6 in numeros
+console.log("resposta: ", confere)
